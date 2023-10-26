6979430db2d18daa7f370d254ae8100b1ae7f95d
rabill.madarboccus <rabill.madarboccus@rogerscapital.mu>
2023-10-26 21:31:08 +0400

    bug fix

--- src/pages/Integration.jsx
+++ src/pages/Integration.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
 import IntegrationArea from '../components/Integration/IntegrationArea';
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useRef, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useRef, useState } from 'react';
 
 const Integration = () => {
   return (
-    <div className= "container-fluid vh-100" style={{ width: '100vw', height: '100vh', background: 'white' , marginTop: '2rem'}}>
+    <div
+      className="container-fluid vh-100"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        background: 'white',
+        marginTop: '2rem',
+      }}
+    >
       <IntegrationArea />
     </div>
   );
