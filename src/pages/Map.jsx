2a0ba1707ae65f7c04597945e97c6c7c22e9e6b4
pravesh14-code <ayushgowreea@gmail.com>
2023-10-26 22:01:34 +0400

    Refining dashboard and map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -58,7 +58,7 @@ const Map = () => {
     });
   }, []);
 
-  return <div id="map" style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}></div>;
+  return <div className="container-fluid vh-100" id="map" style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}></div>;
 };
 
 export default Map;
