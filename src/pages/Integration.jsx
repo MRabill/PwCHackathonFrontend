d1f832b1852f74f670f4f379082d42ece0a046a5
rabill.madarboccus <rabill.madarboccus@rogerscapital.mu>
2023-10-26 22:11:30 +0400

    Style correction

--- src/pages/Integration.jsx
+++ src/pages/Integration.jsx
@@ -10,7 +10,7 @@ const Integration = () => {
         width: '100vw',
         height: '100vh',
         background: 'white',
-        marginTop: '2rem',
+        // marginTop: '2rem',
       }}
     >
       <IntegrationArea />
--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useRef, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/Overview.css";
-import OverviewAPIContainer from "../components/OverviewAPIContainer";
-import { Button, Input, Space, Spin } from "antd"; // Import Spin component
-import { SearchOutlined } from "@ant-design/icons";
-import Highlighter from "react-highlight-words";
-import TableP14 from "../components/TableP14";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Overview.css';
+import OverviewAPIContainer from '../components/OverviewAPIContainer';
+import { Button, Input, Space, Spin } from 'antd'; // Import Spin component
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
+import TableP14 from '../components/TableP14';
+import { Link } from 'react-router-dom';
 
 function Overview() {
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
   const [apiData, setApiData] = useState([]);
   const [loading, setLoading] = useState(true); // Loading state
   const searchInput = useRef(null);
 
   const fetchDataFromAPI = async () => {
     try {
-      const response = await fetch("http://192.168.100.70:5000/integrations");
+      const response = await fetch('http://192.168.100.70:5000/integrations');
       if (response.ok) {
         const data = await response.json();
         setApiData(data);
       } else {
-        console.error("Failed to fetch data from the API");
+        console.error('Failed to fetch data from the API');
       }
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     } finally {
       setLoading(false); // Set loading to false when data fetching is complete
     }
@@ -36,7 +36,13 @@ function Overview() {
   }, []);
 
   const getColumnSearchProps = (dataIndex) => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
       <div
         style={{
           padding: 8,
@@ -47,11 +53,13 @@ function Overview() {
           ref={searchInput}
           placeholder={`Search ${dataIndex}`}
           value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
           onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
           style={{
             marginBottom: 8,
-            display: "block",
+            display: 'block',
           }}
         />
         <Space>
@@ -103,7 +111,7 @@ function Overview() {
     filterIcon: (filtered) => (
       <SearchOutlined
         style={{
-          color: filtered ? "#1677ff" : undefined,
+          color: filtered ? '#1677ff' : undefined,
         }}
       />
     ),
@@ -118,12 +126,12 @@ function Overview() {
       searchedColumn === dataIndex ? (
         <Highlighter
           highlightStyle={{
-            backgroundColor: "#ffc069",
+            backgroundColor: '#ffc069',
             padding: 0,
           }}
           searchWords={[searchText]}
           autoEscape
-          textToHighlight={text ? text.toString() : ""}
+          textToHighlight={text ? text.toString() : ''}
         />
       ) : (
         text
@@ -132,71 +140,71 @@ function Overview() {
 
   const columns = [
     {
-      title: "Integration",
-      dataIndex: "integration",
+      title: 'Integration',
+      dataIndex: 'integration',
       render: (text) => (
-        <a style={{ fontSize: "17px", color: "#002855", fontWeight: "200" }}>
+        <a style={{ fontSize: '17px', color: '#002855', fontWeight: '200' }}>
           {text}
         </a>
       ),
-      ...getColumnSearchProps("integration"),
+      ...getColumnSearchProps('integration'),
     },
     {
-      title: "Status",
-      dataIndex: "status",
+      title: 'Status',
+      dataIndex: 'status',
       render: (text) => (
-        <a style={{ fontSize: "17px", color: "#002855", fontWeight: "200" }}>
-          {text}
-        </a>
-      ),
-    },
-    {
-      title: "Uptime",
-      dataIndex: "uptime",
-      render: (text) => (
-        <a style={{ fontSize: "17px", color: "#002855", fontWeight: "200" }}>
+        <a style={{ fontSize: '17px', color: '#002855', fontWeight: '200' }}>
           {text}
         </a>
       ),
     },
     {
-      title: "Requests",
-      dataIndex: "requests",
+      title: 'Uptime',
+      dataIndex: 'uptime',
       render: (text) => (
-        <a style={{ fontSize: "17px", color: "#002855", fontWeight: "200" }}>
+        <a style={{ fontSize: '17px', color: '#002855', fontWeight: '200' }}>
           {text}
         </a>
       ),
-      defaultSortOrder: "descend",
+    },
+    {
+      title: 'Requests',
+      dataIndex: 'requests',
+      render: (text) => (
+        <a style={{ fontSize: '17px', color: '#002855', fontWeight: '200' }}>
+          {text}
+        </a>
+      ),
+      defaultSortOrder: 'descend',
       sorter: (a, b) => a.requests - b.requests,
     },
     {
-      title: "Efficiency",
-      dataIndex: "efficiency",
+      title: 'Efficiency',
+      dataIndex: 'efficiency',
       render: (text) => (
-        <a style={{ fontSize: "17px", color: "#002855", fontWeight: "200" }}>
+        <a style={{ fontSize: '17px', color: '#002855', fontWeight: '200' }}>
           {text}
         </a>
       ),
     },
     {
-      title: "Flags",
-      dataIndex: "flags",
+      title: 'Flags',
+      dataIndex: 'flags',
       render: (text) => (
-        <a style={{ fontSize: "17px", color: "#002855", fontWeight: "200" }}>
+        <a style={{ fontSize: '17px', color: '#002855', fontWeight: '200' }}>
           {text}
         </a>
       ),
     },
     {
       title: null,
-      dataIndex: "null",
+      dataIndex: 'null',
       render: (text, record) => (
         <Link to={`/dashboard?integration=${record.integration}`}>
           <img
             src="https://i.ibb.co/tY7MTNq/pepicons-pop-dots-x.png"
             alt="Click Me"
-            style={{ width: "28px", height: "28px" }}
+            style={{ width: '28px', height: '28px' }}
           />
         </Link>
       ),
@@ -207,11 +215,11 @@ function Overview() {
     <div className="container-fluid vh-100">
       <div className="row">
         <div className="col">
-          <p className="title_name">Overview</p>
+          {/* <p className="title_name">Overview</p> */}
         </div>
       </div>
 
-      <div className="row" id="container_list">
+      <div className="row" id="container_list" style={{ marginTop: '2rem' }}>
         <OverviewAPIContainer />
       </div>
 
